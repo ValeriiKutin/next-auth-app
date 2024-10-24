9d88dbee7d095eb734461fdd5e2e7858300be1e7
Valerii <valerabulon@gmail.com>
2024-10-24 18:55:15 +0300

    add authentication using clerk

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Link from 'next/link'
 import React from 'react'
 
@@ -12,7 +13,13 @@ export default function Header() {
                 <nav className='flex gap-4'>
                     <Link href='/'>Home</Link>
                     <Link href='/about'>About</Link>
-                    <Link href='/sign-in'>Sign in</Link>
+                    {/* <Link href='/sign-in'>Sign in</Link> */}
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
                 </nav>
             </div>
         </header>
