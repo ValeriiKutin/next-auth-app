7efa72ed9cc5bf0baa1444648b5f0f94710cbfce
Valerii <valerabulon@gmail.com>
2024-11-18 19:30:00 +0200

    fix

--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -1,34 +1,35 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     clerkId: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     firstName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    userName: {
-        type: String,
-        required: true,
+    username: {
+      type: String,
+      required: true,
     },
     avatar: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-
-}, { timestamps: true })
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 
